488305f8fd3c022fa870f15ab1a3d3d9258c406b
amar-techahead <amarendra@techaheadcorp.com>
2017-12-21 01:40:46 +0530

    Demon MVVM
    First Commit

---
+++ MvvmDemo/Models/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MvvmDemo.Models
+{
+    public class Person :INotifyPropertyChanged
+    {
+        public Person()
+        {
+            
+        }
+        public string _name;   
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+                OnPropertyChanged("FullName");
+            }
+        }
+        public string _lastname;
+        public string LastName
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                OnPropertyChanged("LastName");
+                OnPropertyChanged("FullName");
+
+            }
+        }
+
+
+        public string _fullName;
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged( string propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
